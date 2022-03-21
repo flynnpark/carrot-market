e35e2ad5ef83414ca71c509a6fc80dbebb5d3ad1
Flynn <wphestiraid@gmail.com>
2022-03-21 18:21:36 +0900

    3.1 TailwindCSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
+  return <div className={'bg-red-300'}>
+    <h1 className={'text-black'}>Hello Next.js</h1></div>;
 };
 
 export default Home;
