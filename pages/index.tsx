8982636c36c61f8ae3ea15df47fb54d1431c06fb
Flynn <wphestiraid@gmail.com>
2022-04-03 16:36:51 +0900

    패키지 버전 업데이트
    - 리액트 18 정식 버전으로 업데이트
    - NextJS 버전 업데이트
    - 보안 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div className={'bg-red-300'}>
-    <h1 className={'text-black'}>Hello Next.js</h1></div>;
+  return (
+    <div className={'bg-red-300'}>
+      <h1 className={'text-black'}>Hello Next.js</h1>
+    </div>
+  );
 };
 
 export default Home;
