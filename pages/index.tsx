6688b051d2c2c7b5ebee448e4adb18620a62ac93
Flynn <wphestiraid@gmail.com>
2022-05-03 09:21:14 +0900

    5.13 Streams

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
-      <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl hover:bg-orange-500 cursor-pointer transition-colors">
+      <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl hover:bg-orange-500 cursor-pointer transition-colors border-transparent">
         <svg
           className="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
