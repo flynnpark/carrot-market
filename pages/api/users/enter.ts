71be8fbae784e985cec244dcf3f67f769bcdfbda
Flynn <wphestiraid@gmail.com>
2022-05-16 09:05:24 +0900

    9.1 Accounts Logic

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,8 +1,31 @@
+import client from 'libs/server/client';
 import withHandler from 'libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body.email);
+interface EnterRequest extends NextApiRequest {
+  body: {
+    phone: string;
+    email: string;
+  };
+}
+
+async function handler(req: EnterRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+
   res.status(200).end();
 }
 
