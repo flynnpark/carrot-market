4cbe2d505742fad561acf6146221f9fa0fc148a9
Flynn <wphestiraid@gmail.com>
2022-05-24 09:01:28 +0900

    10.4 useUser Refactor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,12 +2,15 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSWR('/api/users/me', fetcher);
+  const { data, error } = useSWR('/api/users/me');
   const router = useRouter();
 
-  //
-  return data;
+  useEffect(() => {
+    if (!data?.success) {
+      router.replace('/enter');
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !error && !data };
 }
