b76d2a03a32ec679cda1ec6fe529db60b36e6cc7
Flynn <wphestiraid@gmail.com>
2022-06-07 09:09:44 +0900

    12.2 Post Detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from 'libs/server/withHandler';
+import client from 'libs/server/client';
+import { withApiSession } from 'libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: parseInt(id.toString(), 10),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          id: true,
+          content: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          agrees: true,
+        },
+      },
+    },
+  });
+
+  res.json({
+    success: true,
+    post,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
