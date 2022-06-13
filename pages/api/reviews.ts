4d72491ccaaef50c2e4bb7e641a5c46d43176c80
Flynn <wphestiraid@gmail.com>
2022-06-14 08:29:47 +0900

    13.1 Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from 'libs/server/withHandler';
+import client from 'libs/server/client';
+import { withApiSession } from 'libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      writtenForId: user?.id,
+    },
+    include: {
+      writtenBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  res.json({
+    success: true,
+    reviews,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
