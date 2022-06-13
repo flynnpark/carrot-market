b20ab3d0a1482099fd55af1a54942574e0bdfced
Flynn <wphestiraid@gmail.com>
2022-06-14 08:39:19 +0900

    13.2 Handlers

---
+++ pages/api/users/me/favorites.ts
@@ -0,0 +1,29 @@
+import withHandler, { ResponseType } from 'libs/server/withHandler';
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from 'libs/server/client';
+import { withApiSession } from 'libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const favorites = await client.favorite.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  res.json({
+    success: true,
+    favorites,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
